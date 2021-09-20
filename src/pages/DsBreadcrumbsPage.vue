2a8097de7b5e1972980dcd113393cf064de784e4
Rick <rjones@digitalseat.com>
2021-09-20 17:50:47 -0500

    Added the api details for Breadcrumbs page

--- src/pages/DsBreadcrumbsPage.vue
+++ src/pages/DsBreadcrumbsPage.vue
@@ -1,14 +1,39 @@
 <template>
   <div>
     <BreadcrumbsCode />
+    <api-details :title="cPropTitle" :c-props="cProps" :c-events="cEvents" />
   </div>
 </template>
 
 <script>
 import BreadcrumbsCode from '@/components/codeblocks/BreadcrumbsCode'
+import ApiDetails from '@/components/ApiDetails'
 export default {
   name: 'DsBreadcrumbsPage',
-  components: {BreadcrumbsCode}
+  components: {
+    BreadcrumbsCode,
+    ApiDetails
+  },
+  data() {
+    return {
+      cPropTitle: 'ds-breadcrumbs',
+      cProps: [
+        {
+          name: 'crumbs',
+          type: 'Array',
+          description: '(required) An array of objects that have a label, routeName and params',
+          default: '[]'
+        },
+      ],
+      cEvents: [
+        {
+          name: 'selected',
+          description: 'Triggered when a breadcrumb is selected',
+          value: '{ label, routeName, params }'
+        }
+      ]
+    }
+  }
 }
 </script>
 
